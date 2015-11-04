48f5a7869bf68b1998750166c10a633d01d34ea6
Yuri <yuri_dias_@hotmail.com>
2015-11-04 14:30:15 -0300

    it is wrong

--- db/migrate/20151031200710_add_user_id_and_group_id_to_members.rb
+++
@@ -1,6 +0,0 @@
-class AddUserIdAndGroupIdToMembers < ActiveRecord::Migration
-  def change
-    add_foreign_key :members, :users, column: :user_id, primary_key: :id, on_delete: :cascade
-    add_foreign_key :members, :groups, column: :group_id, primary_key: :id, on_delete: :cascade
-  end
-end

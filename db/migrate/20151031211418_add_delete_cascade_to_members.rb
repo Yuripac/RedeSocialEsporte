5be36e68e8543c8d5320c8e7850bc0602da1f129
Yuri <yuri_dias_@hotmail.com>
2015-11-04 14:35:41 -0300

    it is wrong

--- db/migrate/20151031211418_add_delete_cascade_to_members.rb
+++
@@ -1,6 +0,0 @@
-class AddDeleteCascadeToMembers < ActiveRecord::Migration
-  def change
-    add_foreign_key :members, :users, column: :user_id, primary_key: :id
-    add_foreign_key :members, :groups, column: :group_id, primary_key: :id
-  end
-end
